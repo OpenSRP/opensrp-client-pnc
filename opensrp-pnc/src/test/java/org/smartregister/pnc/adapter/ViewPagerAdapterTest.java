3a050d502dc9b25658a7f162daaa56cfd297362a
vend <12345>
2020-07-23 12:26:48 +0500

    :hammer: WIP

--- opensrp-pnc/src/test/java/org/smartregister/pnc/adapter/ViewPagerAdapterTest.java
+++ opensrp-pnc/src/test/java/org/smartregister/pnc/adapter/ViewPagerAdapterTest.java
@@ -1,7 +1,8 @@
 package org.smartregister.pnc.adapter;
 
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 
 import org.junit.Before;
 import org.junit.Test;
