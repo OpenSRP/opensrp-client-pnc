e91c1ecb93edabb0fb2c0a551556bb4a0bb42fb7
bennsimon <botieno@ona.io>
2020-09-14 15:51:42 +0300

    add test to pnc-medic-info-form processing

---
+++ opensrp-pnc/src/test/java/org/smartregister/pnc/BaseRobolectricTest.java
@@ -0,0 +1,12 @@
+package org.smartregister.pnc;
+
+import android.os.Build;
+
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(sdk = Build.VERSION_CODES.P)
+public abstract class BaseRobolectricTest {
+}
